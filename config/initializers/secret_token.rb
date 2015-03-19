fa612fb3cfaf60e9ccb988e2f722f0b81bb2b1f5
PeoneEr <peoneerko@yandex.ru>
2015-03-19 17:21:05 +0600

    secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Storage::Application.config.secret_token = Settings['app.secret']
+Storage::Application.config.secret_token = '3ac604f39048697fe715fc0a77ab4af1a474171ea056b69c7835f95c9bd2526923dce8f503ee9c8c3c64e3e26788d15cb5d7a84ffef6bfb91d6d9daa4d517d38'
